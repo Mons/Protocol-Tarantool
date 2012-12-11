5913690c8d362ae5c8b8a2501a203b6d75f58c1f
Mons Anderson <mons@cpan.org>
2012-12-11 17:17:43 +0400

    fix meta

--- makeall.sh
+++ makeall.sh
@@ -4,7 +4,7 @@ MODULE=`perl -ne 'print($1),exit if m{version_from.+?([\w/.]+)}i' Makefile.PL`;
 perl=perl
 $perl -v
 
-rm -rf MANIFEST.bak Makefile.old && \
+rm -rf MANIFEST.bak Makefile.old MYMETA.* META.* && \
 pod2text $MODULE > README && \
 $perl -i -lpne 's{^\s+$}{};s{^    ((?: {8})+)}{" "x(4+length($1)/2)}se;' README && \
 AUTHOR=1 $perl Makefile.PL && \
